561fb3c95db9a1407014e83559c505ff15dc7c0c
jordannathanael <jnat0012@student.monash.edu>
2023-02-01 13:53:27 +1100

    Documentation of Player.py

--- player.py
+++ player.py
@@ -94,6 +94,22 @@ class Player():
     
     """
     NOTE: unless specified all methods have a best and worst case complexity of O(1)
+
+    Class Attributes:
+        DEFAULT_EMERALDS: Default amount of emeralds that player hold in the first round
+        MIN_EMERALDS: Lower bound of amount of emeralds when initialize the player
+        MAX_EMERALDS: upper bound of amount of emeralds when initialize the player
+
+    Attribute:
+        name: name of the player
+        balance: emerald balance that the player have
+        hunger: Hunger indicator for the player
+        trader: Trader that player wants to trade with
+        food_list: List of foods that player can choose from
+        caves_list: List of caves that player can choose from
+
+        AI: mode of the player (True/False)
+        key_cooldown: cooldown time
     """
 
     DEFAULT_EMERALDS = 50
@@ -102,6 +118,7 @@ class Player():
     MAX_EMERALDS = 40
 
     def __init__(self, name, emeralds=None, **kwargs) -> None:
+        """ Initialization """
         self.name = name
         self.set_balance(self.DEFAULT_EMERALDS if emeralds is None else emeralds)
         self.set_hunger() 
@@ -116,14 +133,17 @@ class Player():
             self.AI = True
         
     def __str__(self) -> str:
+        """ Formatted string representation """
         return f"{self.name}: {self.get_balance()}💰"
     
     def __repr__(self):
+        """ Formatted string represntation """
         return self.__str__()
     
     # class methods
     @classmethod
     def random_player(self) -> Player:
+        """ Create the Player object """
         return Player(RandomGen.random_choice(PLAYER_NAMES), RandomGen.randint(self.MIN_EMERALDS, self.MAX_EMERALDS))
 
     # mutators
@@ -132,26 +152,31 @@ class Player():
         pass
     
     # hunger
-    def set_hunger(self, hunger: float = 0) -> None: 
+    def set_hunger(self, hunger: float = 0) -> None:
+        """ Set the hunger value for player """
         if isinstance(hunger, int) or isinstance(hunger, float):
             self.hunger = round(hunger, 2)
         else:
             self.hunger = hunger
         
     def get_hunger(self) -> float:
+        """ Get the hunger value of the player """
         self.set_hunger(self.hunger) # makes sure its rounded to two decimal places
         return self.hunger
     
-    def decrease_hunger(self, hunger: float = 0) -> None: 
+    def decrease_hunger(self, hunger: float = 0) -> None:
+        """ Decrease the hunger once player consume food """
         if isinstance(hunger, int) or isinstance(hunger, float):
             self.hunger -= round(hunger, 2)
         else:
             self.hunger -= hunger
     
     def clear_hunger(self):
+        """ Reset hunger value into zero """
         self.set_hunger(0)
         
     def check_hunger(self):
+        """ Check whether """
         if self.hunger <= 0:
             self.clear_hunger()
             return True
@@ -160,38 +185,39 @@ class Player():
     
     # balance
     def set_balance(self, balance: float = 0) -> None:
+        """ Rounding the emerald balance of the player by 2 number behind the decimal """
         if isinstance(balance, int) or isinstance(balance, float):
             self.balance = round(balance, 2)
         else:
             self.balance = balance
         
     def get_balance(self) -> float:
+        """ Get the emerald balance of the player"""
         self.set_balance(self.balance) # makes sure its rounded to two decimal places
         return self.balance
     
     def increase_balance(self, balance: float = 0):
-        if isinstance(balance, int) or isinstance(balance, float):
-            self.balance += round(balance, 2)
-        else:
-            self.balance += balance
-            
+        """ Increase emerald balance of the player"""
+        self.set_balance(self.balance + balance)
+
     def decrease_balance(self, balance: float = 0):
-        if isinstance(balance, int) or isinstance(balance, float):
-            self.balance -= round(balance, 2)
-        else:
-            self.balance -= balance
+        """ Decrease emerald balance of the player """
+        self.set_balance(self.balance - balance)
     
 
     # traders
     def set_traders(self, traders_list: list[Trader] = None) -> None:
+        """ Set the trader for each turn """
         self.traders_list = traders_list
         
     def get_traders(self) -> list[Trader]:
+        """ Get the list of traders """
         return self.traders_list 
     
     
     # foods
     def set_foods(self, foods_list: list[Food] = None) -> None:
+        """ Set the list of foods that is provided for player """
         if foods_list != None:
             self.foods_list: list[Food] = [food for food in foods_list if isinstance(food, Food)]
         elif self.get_traders() != None:
@@ -200,81 +226,73 @@ class Player():
             self.foods_list = foods_list
             
     def get_foods(self) -> list[Food]:
+        """ Get the list of foods as an option for player """
         return self.foods_list     
     
     # caves
     def set_caves(self, caves_list: list[Cave] = None) -> None:
+        """ Set the list of caves that is provided for player """
         self.caves_list = caves_list
 
     def get_caves(self) -> list[Cave]:
+        """ Get the list of caves"""
         return self.caves_list 
 
     # (SOLO) select_food_and_caves
     def select_food_and_caves(self, offered_food: Food = None) -> tuple[Food | None, float, list[tuple[Cave, float]]]: 
-        """_summary_
+        """
         
         Returns :
             tuple: _description_
-        
-        brief on function:
-Given that the player has access to the materials, caves, and traders given through the relevant methods in the player class (which you need to implement), 
-we want to implement the method select_food_and_caves, which generates a tuple containing 3 objects:
-- The food which this player will buy
-- The emerald balance after this player has made their move
-- A list of all caves plundered on their journey, paired with the quantity of each material mined.
 
-The choice the player makes should be optimal (Achieving the highest balance of emeralds possible in a single day) The method should not change the existing quantities of any caves, or any statistics of any material/cave/trader object.
+        Approaches:
+        SOLO food:
+            Returns the food that has the most hunger filling while the balance is above 0
 
-Complexity Requirement!
-Given that F = #Foods, T = #Traders, C = #Caves, M = #Materials:
-Your select_food_and_caves method should have complexity at most O(M + T + F * C * logC)
+        SOLO caves:
+            Returns caves in order of mining based on how much profit you can make from them
 
-Documentation Requirement!
-For your solution to select_food_and_caves, please leave a lengthy docstring describing the motivation for your approach in full. 
-Please use a small example to demonstrate your approach. Additionally, you need to fully justify the complexity of your approach - Give line comments to summarise the complexity of blocks of your code.
-
-Approaches:
-SOLO food:
-    Returns the food that has the most hunger filling while the balance is above 0
-
-SOLO caves:
-    Returns caves in order of mining based on how much profit you can make from them
-    
-SOLO Complexity: (best & worst) = O(T + F + C)
+        SOLO Complexity: (best & worst) = O(T + F + C)
 
 
-MULTIPLAYER food:
-    If the player can afford the food, they buy it
+        MULTIPLAYER food:
+            If the player can afford the food, they buy it. O(1)
 
-MULTIPLAYER caves:
-    As you can only choose one cave, you want the most amount of profit possible from a single cave
+        MULTIPLAYER caves:
+            As you can only choose one cave, you want the most amount of profit possible from a single cave
 
-MULTIPLAYER Complexity: (best & worst) = O(C + T)
+        MULTIPLAYER Complexity: (best & worst) = O(C + T)
         """
         self.generate_material_price_map() # O(T)
-        
+
+        # if the offered food only one
+        # refers to multiplayer gameplay
         if isinstance(offered_food, Food):
             if self.AI:
-                chosen_food, chosen_caves = self.multiplayer_AI_select_food_and_caves(offered_food)
+                chosen_food, chosen_caves = self.multiplayer_AI_select_food_and_caves(offered_food) # O(C)
             else:
-                chosen_food, chosen_caves = self.get_multiplayer_real_player_choices(offered_food)
+                chosen_food, chosen_caves = self.get_multiplayer_real_player_choices(offered_food) # O(C)
+
+        # singleplayer
         else:
             if self.AI:
-                chosen_food, chosen_caves = self.AI_select_food_and_caves()
+                chosen_food, chosen_caves = self.AI_select_food_and_caves() #O(F+C)
             else:
-                chosen_food, chosen_caves = self.get_solo_real_player_choices()
+                chosen_food, chosen_caves = self.get_solo_real_player_choices() #O(F+C)
     
         return (chosen_food, self.get_balance(), chosen_caves)
     
     def AI_select_food_and_caves(self) -> tuple[Food, list[Cave]]:
-        chosen_food = self.choose_food()
-        chosen_caves = self.choose_caves()
+        """ Choose the food and caves if AI mode is True for solo gameplay """
+        chosen_food = self.choose_food() #O(F)
+        chosen_caves = self.choose_caves() #O(C)
         return chosen_food, chosen_caves
     
     def multiplayer_AI_select_food_and_caves(self, offered_food) -> tuple[Food|None, Cave]:
-        chosen_food = self.multiplayer_choose_food(offered_food)
+        """ Choose the food and caves if AI mode is True and in multiplayer mode game """
+        chosen_food = self.multiplayer_choose_food(offered_food) #O(1)
         chosen_cave = self.multiplayer_choose_caves()
-        return chosen_food, chosen_cave
+        return chosen_food, chosen_cave #O(C)
     
     # SOLO
     
@@ -361,6 +379,7 @@ MULTIPLAYER Complexity: (best & worst) = O(C + T)
     # helper methods
     
     def get_material_price(self, material: Material) -> float:
+        """ Get the price of that particular material """
         try:
             return self.material_price_map[f"{material}"]
         except KeyError:
@@ -394,6 +413,8 @@ MULTIPLAYER Complexity: (best & worst) = O(C + T)
     # SOLO 
     
     def get_solo_real_player_choices(self):
+        """ Get the choice from player in singleplayer mode
+        """
         while True:
             try:
                 try:
@@ -409,6 +430,7 @@ MULTIPLAYER Complexity: (best & worst) = O(C + T)
             
             
     def choose_food_screen(self):
+        """ UI to choose the food """
         raise NotImplementedError
         clearConsole()
         self.title = 'Food'
@@ -419,6 +441,7 @@ MULTIPLAYER Complexity: (best & worst) = O(C + T)
         
         
     def choose_cave_screen(self):
+        """ UI to choose the cave """
         raise NotImplementedError
         clearConsole()
         self.title = 'Caves'
@@ -430,6 +453,8 @@ MULTIPLAYER Complexity: (best & worst) = O(C + T)
     
     # MULTI
     def get_multiplayer_real_player_choices(self, offered_food: Food):
+        """ Ask input from user in multiplayer mode
+        """
         while True:
             try:
                 try:
@@ -444,6 +469,7 @@ MULTIPLAYER Complexity: (best & worst) = O(C + T)
                 raise e 
     
     def multiplayer_choose_food_screen(self, offered_food):
+        """ UI to show the food option in multiplayer mode """
         raise NotImplementedError
         clearConsole()
         self.title = 'Food'
@@ -453,6 +479,8 @@ MULTIPLAYER Complexity: (best & worst) = O(C + T)
        
         
     def multiplayer_choose_cave_screen(self):
+        """ UI to show the cave option in multiplayer mode
+        """
         raise NotImplementedError
         clearConsole()
         self.title = 'Caves'
@@ -465,6 +493,7 @@ MULTIPLAYER Complexity: (best & worst) = O(C + T)
     # SOLO + MULTI METHODS
     
     def display_title(self):
+        """ UI to show the title of the game """
         while True:
             try:
                 title = self.title
