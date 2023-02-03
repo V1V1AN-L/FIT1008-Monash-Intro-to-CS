a9e9c0d5dee9cdaddb94c0309de459d64856ce68
Densetsu152637 <104627128+Densetsu152637@users.noreply.github.com>
2023-02-03 21:49:48 +1100

    yeet

--- MaterialSet.py
+++ MaterialSet.py
@@ -7,11 +7,11 @@ class MaterialSet(ASet):
     """
 
     def init(self, capacity: int = 1, items: list[Material] = None) -> None:
-        super().__init__()
+        ASet.__init__(self)
         self.array = ArrayR(max(1, capacity))
         if isinstance(items, list):
             for material in items:
                 self.add(material)
         
     def get_list(self) -> list[Material]:
-        return [material for material in self.array if material is not None]
+        return self.array.array[:self.size]
