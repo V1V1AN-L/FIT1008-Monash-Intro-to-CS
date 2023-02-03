895547720edab628751b1b6e4513b7b621d69586
Densetsu152637 <104627128+Densetsu152637@users.noreply.github.com>
2023-02-03 21:39:24 +1100

    Update MaterialSet.py

--- MaterialSet.py
+++ MaterialSet.py
@@ -1,4 +1,5 @@
 from aset import *
+from material import *
 
 class MaterialSet(ASet):
     """
@@ -8,39 +9,6 @@ class MaterialSet(ASet):
     def init(self, capacity: int = 1) -> None:
         super().__init__()
         self.array = ArrayR(max(1, capacity))
-
-
-    def clear(self) -> None:
-        self.size = 0
-
-    def len(self) -> int:
-        return self.size
-
-    def is_empty(self) -> bool:
-        return len(self) == 0
-
-    def is_full(self) -> bool:
-        return len(self) == len(self.array)
-
-    def contains(self, item: T) -> bool:
-        for i in range(self.size):
-            if item == self.array[i]:
-                return True
-        return False
-
-    def add(self, item: T) -> None:
-        if item not in self:
-            if self.is_full():
-                raise Exception("Set is full")
-
-            self.array[self.size] = item
-            self.size += 1
-
-    def remove(self, item: T) -> None:
-        for i in range(self.size):
-            if item == self.array[i]:
-                self.array[i] = self.array[self.size - 1]
-                self.size -= 1
-                break
-        else:
-            raise KeyError(item)
+        
+    def get_list(self) -> list[Material]:
+        return [material for material in self.array if material is not None]
