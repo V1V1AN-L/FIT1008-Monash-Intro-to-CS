97c76b6b65030521161eec3ed8ef82ec6f0ad807
Densetsu152637 <104627128+Densetsu152637@users.noreply.github.com>
2023-02-03 21:55:37 +1100

    Update MaterialSet.py

--- MaterialSet.py
+++ MaterialSet.py
@@ -15,3 +15,24 @@ class MaterialSet(ASet):
         
     def get_list(self) -> list[Material]:
         return self.array.array[:self.size]
+    
+    def add(self, item: T) -> None:
+        """ Adds an element to the set. Note that an element already
+        present in the set should not be added.
+        :pre: the set is not full
+        :raises Exception: if the set is full.
+        """
+        if self.is_full():
+            self.refactor_array()
+        if item not in self:
+            self.array[self.size] = item
+            self.size += 1
+            
+            
+    def refactor_array(self):
+        # refactor array
+        old_array = self.array
+        new_array = ArrayR(len(old_array) + 10)
+        self.array = new_array
+        for i in range(len(old_array)):
+            self.array[i] = old_array[i]
