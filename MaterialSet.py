953636d0b6685fd023ec6e93b921db774ceb35c2
Densetsu152637 <104627128+Densetsu152637@users.noreply.github.com>
2023-02-03 21:44:47 +1100

    yeet

--- MaterialSet.py
+++ MaterialSet.py
@@ -6,9 +6,12 @@ class MaterialSet(ASet):
     Set ADT for storing the trader's materials
     """
 
-    def init(self, capacity: int = 1) -> None:
+    def init(self, capacity: int = 1, items: list[Material] = None) -> None:
         super().__init__()
         self.array = ArrayR(max(1, capacity))
+        if isinstance(items, list):
+            for material in items:
+                self.add(material)
         
     def get_list(self) -> list[Material]:
         return [material for material in self.array if material is not None]
